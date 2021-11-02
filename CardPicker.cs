211550052d2f8fca70772d29aa4e0fbce9d31bf1
mbalandari <m.balandari.toroghi@gmail.com>
2021-11-02 13:49:13 +0300

    Add project files.

---
+++ CardPicker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string RandomValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
